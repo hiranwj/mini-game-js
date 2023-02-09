0122ea3786af8b3ac257110e7924cb10425a33d1
hiranwj <hiranwj.connect@gmail.com>
2023-02-10 00:41:58 +0530

    Add eventListeners key up and key down

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log('Working');
+// const player = document.querySelector('#player');
+const player = document.getElementById('player');
+
+
+addEventListener('keydown', ({ key }) => {
+    console.log('Key DOWN');
+});
+
+addEventListener('keyup', ({ key }) => {
+    console.log('Key UP');
+});
