41ce76eee4911d27bc63c8602696f15f79b2113b
hiranwj <hiranwj.connect@gmail.com>
2023-02-10 00:45:05 +0530

    Add ArrowLeft and ArrowRight

--- js/index.js
+++ js/index.js
@@ -3,9 +3,15 @@ const player = document.getElementById('player');
 
 
 addEventListener('keydown', ({ key }) => {
-    console.log('Key DOWN');
+    // console.log('Key DOWN');
+    if (key === "ArrowRight") {
+        console.log('ArrowRight');
+    }
 });
 
 addEventListener('keyup', ({ key }) => {
-    console.log('Key UP');
+    // console.log('Key UP');
+    if (key === "ArrowLeft") {
+        console.log('ArrowLeft');
+    }
 });
