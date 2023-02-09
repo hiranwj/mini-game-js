779069cb821c22211062e14becdf7e496192973b
hiranwj <hiranwj.connect@gmail.com>
2023-02-10 02:11:31 +0530

    Completed move the square

--- js/index.js
+++ js/index.js
@@ -1,11 +1,36 @@
-// const playerElm = document.querySelector('#player');
-const playerElm = document.getElementById('player');
-console.log(playerElm);
+// const player = document.getElementById('player');
+const player = document.querySelector('#player');
+
+// const ground = document.getElementById('ground');
+const ground = document.querySelector('#ground');
+
 let dx = 0;
+let dy = 5;
+const acceleration = 0.1;
 
 setInterval(()=>{
-    playerElm.style.left = `${playerElm.offsetLeft + dx}px`
-}, 17); 
+
+    if ((player.offsetLeft + player.offsetWidth) > innerWidth) {
+        dx = 0;
+        player.style.left = `${innerWidth - player.offsetWidth}px`;
+    }
+    else if (player.offsetLeft < 0) {
+        dx = 0;
+        player.style.left = '0';
+    }
+
+    if ((player.offsetTop + player.offsetHeight) > ground.offsetTop) {
+        dy = 0;
+        player.style.top = `${ground.offsetTop - player.offsetHeight}px`;
+    }
+    else {
+        dy += acceleration;
+    }   
+
+    player.style.left = `${player.offsetLeft + dx}px`
+    player.style.top = `${player.offsetTop + dy}px`;
+}, 17);
+
 /* 17 - frame rate */
 
 addEventListener('keydown', ({ key }) => {
@@ -16,10 +41,17 @@ addEventListener('keydown', ({ key }) => {
     else if (key === "ArrowLeft") {
         dx = -10;
     }
+    else if(key === "ArrowUp") {
+        dy = -20;
+    }
 });
 
 addEventListener('keyup', ({ key }) => {
     if (key === "ArrowRight" || key === "ArrowLeft") {
         dx = 0;
+    }
+
+    if(key == "ArrowUp") {
+        dy = 0;
     }
 });
